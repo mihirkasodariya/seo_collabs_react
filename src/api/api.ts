de1f68b10842f0a7ae9dad34a7fe599a3d1acb2b
Mihir Kasodariya <mihirkasodariya21@gmail.com>
2025-11-13 13:22:27 +0530

    route issue fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL+ "/api",
 });
 
 // Request Interceptor
