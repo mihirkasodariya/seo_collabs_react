7736b1324789fa223fd7c7e9e8b8eba73eee6f0f
Mihir Kasodariya <mihirkasodariya21@gmail.com>
2025-11-13 13:28:04 +0530

    vite config file chage route issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,29 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// // Get __dirname equivalent in ES modules
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     host: true,
+//     port: 1500,
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   optimizeDeps: {
+//     include: ["three"],
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -10,6 +36,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", // ✅ important for Vercel (handles routing correctly)
   server: {
     host: true,
     port: 1500,
